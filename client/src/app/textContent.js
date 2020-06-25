178b1457f18947a084457d7cf958a26f0d882657
Hans Vertriest <hansvertriest@MacBook-Pro.local>
2020-06-25 11:20:49 +0200

    added textContent

---
+++ client/src/app/textContent.js
@@ -0,0 +1,34 @@
+const text = {
+    "en_brand" : {
+        header_title: "The right person for your business",
+        header_text: "A breakthrough in creating intuitive new opportunities to inspire others through influencer marketing.",
+
+        section_one_title: "Everybody influences",
+        section_one_text_one: "In a society where social media is more present than ever, it’s not the question who the influencers are. These days you don’t need 10.000 followers to call yourself an  influencer.",
+        section_one_highlight: "Even if you have only 800 or 900 influencers you can use our app.",
+        section_one_text_two: "Connus helps you to grow your influencing carreer by connecting you with a brand or  product who fits your content.",
+
+        section_two_title: "Nothing like an agency",
+        section_two_text_one: "Nation against nation, different kinds of nationality.",
+        section_two_text_two: "People are proud  of being part of an eco-system (I am proud to be a Belgian) à I want to buy things  that has an influence on other ones. This is not about you, but it’s about the influence you have.  We see you and how you can change the world. ",
+        section_two_text_three: "Connecting, seeing you …  We need each other! Growing together, building together, everything together …",
+        section_two_bubble_two_text: "<strong>Merken</strong> zijn al geregistreerd",
+        section_two_bubble_four_text: "<strong>Influencers</strong> zijn al geregistreerd",
+        section_two_bubble_five_text: "Geregistreerde <strong>matches</strong>",
+
+        section_three_title: "Find out what influencer you are",
+        section_three_followers: "followers",
+        section_three_reach: "Reach",
+        section_three_engagement: "Engagement",
+        section_three_complexity: "Complexity",
+        section_three_trust: "Trust",
+        section_three_cost: "Cost",
+        
+        section_five_title: "Get payed to use our app",
+        section_five_content: "This ‘click-based’ advertising gives you the security that you pay when your brand raises it’s brand-awareness or when you connect to influencers.",
+
+        section_six_proudPartner: "Proud partner of",
+
+        section_six_doanload_title: "Are your ready to start your influencer carreer right now",
+    }
+}
