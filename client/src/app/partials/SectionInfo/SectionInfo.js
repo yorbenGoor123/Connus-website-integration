fd880e4345d813593cc6f3100ba0714f1e7b618a
yorbenGoor123 <yorbengoor5@gmail.com>
2020-07-28 20:26:12 +0200

    added lines

--- client/src/app/partials/SectionInfo/SectionInfo.js
+++ client/src/app/partials/SectionInfo/SectionInfo.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import style from './SectionInfo.module.css'
+import left from '../../assets/detailpages/01-content-creator/left.svg';
+import right from '../../assets/detailpages/01-content-creator/right.svg';
 
 const SectionInfo = ({direction, title, text, illustration, marginLeft, marginRight}) => {
     return (
+        <>
         <div className= {(direction === "left" ? style.detailPageSectionInfo__wrapper: style.directionLeft)}>
             <div className={style.detailPageSectionInfo__paragraphs}>
                 <h2 className={style.detailPageSectionInfo__title}>{title}</h2>
@@ -11,6 +14,15 @@ const SectionInfo = ({direction, title, text, illustration, marginLeft, marginRi
             </div>
             <img  style={{marginLeft: `${marginLeft}`, marginRight:`${marginRight}`}} className={style.illustration}  src={require(`../../assets/detailpages/01-content-creator/illustration${illustration}.svg`)} alt="imageConnect"></img>
         </div>
+        <div className={style.overflow}>
+        {direction === "left" ? (
+            <img src={left} alt="left"></img>
+        ):
+            <img src={right} alt="right"></img>
+        }
+        
+        </div>
+        </>
     );
 };
 
